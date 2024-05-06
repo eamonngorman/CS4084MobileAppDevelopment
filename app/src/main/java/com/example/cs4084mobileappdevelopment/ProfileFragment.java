814ee9f498b5681f7d5acb25dd610e9bbd772810
jamespb20 <skeletonporkchop20@gmail.com>
2024-05-06 16:13:01 +0100

    started profile fragment

---
+++ app/src/main/java/com/example/cs4084mobileappdevelopment/ProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.cs4084mobileappdevelopment;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileFragment extends Fragment{
+    FirebaseAuth auth;
+    FirebaseUser user;
+
+}
