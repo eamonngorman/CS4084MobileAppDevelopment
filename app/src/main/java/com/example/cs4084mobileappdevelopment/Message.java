94a1fc9fbb523cb8639501b93a04704bc9f02565
Eamonn <eamonngorman21@gmail.com>
2024-05-07 11:55:01 +0100

    Implementation of upvote/downvote system for RecyclerViewFragment

--- app/src/main/java/com/example/cs4084mobileappdevelopment/Message.java
+++ app/src/main/java/com/example/cs4084mobileappdevelopment/Message.java
@@ -1,16 +1,24 @@
 package com.example.cs4084mobileappdevelopment;
 
+import android.util.Log;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Message {
     private String message;
     private double latitude;
     private double longitude;
     private String category;
+    private String postId;
 
     public Message() {
         // Default constructor required for Firestore
     }
 
-    public Message(String message, double latitude, double longitude, String category) {
+    public Message(String postId, String message, double latitude, double longitude, String category) {
+        this.postId = postId;
         this.message = message;
         this.latitude = latitude;
         this.longitude = longitude;
@@ -48,5 +56,34 @@ public class Message {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public DocumentReference getPostRef() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        CollectionReference postsRef = db.collection("messages");
+
+        // Log postId for debugging
+        Log.d("Message", "getPostRef: PostId: " + this.postId);
+
+        // Get a reference to the specific post using its postId
+        DocumentReference postReference = postsRef.document(this.postId);
+
+        // Log the post reference for debugging
+        if (postReference != null) {
+            Log.d("Message", "getPostRef: Post Reference: " + postReference.getPath());
+        } else {
+            Log.d("Message", "getPostRef: Post Reference is null");
+        }
+
+        return postReference;
     }
 }
